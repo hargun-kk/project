c650961f69d9d4a4e2ac4f2ae6ecc371bd15e7b0
hargun-kk <khargundeep000@gmail.com>
2025-11-08 11:03:26 -0500

    Update Menu.h

--- Menu.h
+++ Menu.h
@@ -1,1 +1,17 @@
+#indef SENECA_MENU_H
+#define SENECA_MENU_H
 
+namespace seneca {
+
+
+    class MenuItem{
+          char* MenuItemContent;
+          size_t indentations;
+          size_t size;
+          int numbers;
+     }
+public:
+    MenuItem(const char* MenuItemContent, int numIndent, int indentSize, int rowNum);
+    
+};
+    
